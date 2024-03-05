238fea35268ed807f3caf9d354b1b8915bfb5b3a
Chronik <vladislamakarevich@mail.ru>
2024-03-06 00:00:39 +0300

    домашнее задание по js 12

--- data.js
+++ data.js
@@ -4,37 +4,61 @@ const product = `
         "img": "./img/Catalog1.png",
         "title": "ELLERY X M'O CAPSULE",
         "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "prise": "$52.00"
+        "prise": "$52.00",
+        "heading": "JACKET WITH HOOD",
+        "color": "Dark blue",
+        "size": "XL",
+        "quantity": "1"
     },
     {
         "img": "./img/Catalog2.png",
         "title": "ELLERY X M'O CAPSULE",
         "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "prise": "$52.00"
+        "prise": "$52.00",
+        "heading": "WOMEN'S DRESS",
+        "color": "Black",
+        "size": "XL",
+        "quantity": "1"
     },
     {
         "img": "./img/Catalog3.png",
         "title": "ELLERY X M'O CAPSULE",
         "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "prise": "$52.00"
+        "prise": "$52.00",
+        "heading": "MANGO PEOPLE T&nbsp;-&nbsp;SHIRT",
+        "color": "Red",
+        "size": "XL",
+        "quantity": "1"
     },
     {
         "img": "./img/Catalog4.png",
         "title": "ELLERY X M'O CAPSULE",
         "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "prise": "$52.00"
+        "prise": "$52.00",
+        "heading": "MEN'S TAPERED TROUSERS",
+        "color": "Yellow",
+        "size": "XL",
+        "quantity": "1"
     },
     {
         "img": "./img/Catalog5.png",
         "title": "ELLERY X M'O CAPSULE",
         "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "prise": "$52.00"
+        "prise": "$52.00",
+        "heading": "JACKETS FOR WOMEN",
+        "color": "heavenly",
+        "size": "XL",
+        "quantity": "1"
     },
     {
         "img": "./img/Catalog6.png",
         "title": "ELLERY X M'O CAPSULE",
         "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "prise": "$52.00"
+        "prise": "$52.00",
+        "heading": "WOMEN'S SHIRT",
+        "color": "brown",
+        "size": "XL",
+        "quantity": "1"
     }
 ]
 `;
